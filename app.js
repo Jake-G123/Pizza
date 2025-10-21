a76132e147cffa530e970e35dc68386b6fa44d53
Jake-G123 <jakelgerken@gmail.com>
2025-10-21 11:19:44 -0700

    added new routes

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 // Enable static file serving
 app.use(express.static('public'));
 
+// Allow the app to parse form data (req.body)
+app.use(express.urlencoded({ extended: true }));
+
+//create an array to store orders
+const orders = [];
+
 // Define the port number where our server will listen
 const PORT = 3001;
 
@@ -18,6 +24,42 @@ app.get('/', (req, res) => {
     // Send a response to the client
     // res.send(`<h1>Welcome to Poppa\'s Pizza!</h1>`);
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// define contact us root
+app.get('/contact-us', (req, res) => { // root can be a dif name than the .html file
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+// define confirmation us root
+app.get('/confirm', (req, res) => { // root can be a dif name than the .html file
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// define confirmation us root
+app.get('/admin', (req, res) => { // root can be a dif name than the .html file
+
+    res.send(orders);
+    //res.sendFile(`${import.meta.dirname}/views/admin.html`);
+});
+
+app.post('/submit-order', (req, res) => { // root can be a dif name than the .html file
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+
+    // console.log(req.body);
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings,
+        size: req.body.size,
+        comment: req.body.comment
+    };
+
+    // Array of orders
+    orders.push(order);
+    console.log(orders);
 });
 
 // Start the server and make it listen on the port 
